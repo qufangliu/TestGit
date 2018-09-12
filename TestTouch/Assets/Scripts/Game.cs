cf410df8c829d4ecabf7c013b0e962da027c9a78
qufangliu <qufangliu@qufangliudeMac-mini.local>
2018-09-12 14:43:00 +0800

    my git test

---
+++ TestTouch/Assets/Scripts/Game.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+	private GameObject targetObj;
+	private GameObject previweObj;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetMouseButtonDown( 0 ) )
+		{
+			targetObj = GetTouchedObject( Input.mousePosition );
+			if( targetObj != null )
+			{
+				previweObj = CloneObject( targetObj );
+			}
+		}
+
+		if( Input.GetMouseButton( 0 ) )
+		{
+			if( previweObj != null )
+			{
+				UpdatePreview( Input.mousePosition );
+			}
+		}
+		
+		if( Input.GetMouseButtonUp( 0 ) )
+		{
+			if( previweObj != null )
+			{
+				Destroy( previweObj );
+				previweObj = null;
+			}
+		}
+	}
+
+	private GameObject GetTouchedObject( Vector2 point )
+	{
+		RaycastHit hit;
+		var ray = Camera.main.ScreenPointToRay( point );
+		var success = Physics.Raycast( ray, out hit, float.MaxValue );
+		if( success )
+		{
+			return hit.collider.gameObject;
+		}
+		return null;
+	}
+
+	private GameObject CloneObject( GameObject obj )
+	{
+		var preview = Instantiate( obj );
+		preview.gameObject.name = "preview_" + obj.gameObject.name;
+		preview.layer = LayerMask.NameToLayer( "DragObject" );
+		return preview;
+	}
+
+	private void UpdatePreview( Vector2 point )
+	{
+		var distance = Vector3.Distance( targetObj.transform.position, Camera.main.transform.position );
+		var pos = new Vector3(point.x, point.y, Camera.main.nearClipPlane);
+		var posInView = Camera.main.ScreenToViewportPoint( pos );
+		posInView.z = distance;
+		previweObj.transform.position = Camera.main.ViewportToWorldPoint( posInView );
+	}
+}
---
+++ TestTouch/Assets/Shaders/LWRP/InputSurfaceSimpleWithHair.hlsl
@@ -0,0 +1,44 @@
+#ifndef LIGHTWEIGHT_INPUT_SURFACE_SIMPLE_INCLUDED
+#define LIGHTWEIGHT_INPUT_SURFACE_SIMPLE_INCLUDED
+
+#include "LWRP/ShaderLibrary/Core.hlsl"
+#include "LWRP/ShaderLibrary/InputSurfaceCommon.hlsl"
+
+CBUFFER_START(UnityPerMaterial)
+float4 _MainTex_ST;
+half4 _Color;
+half4 _SpecColor;
+half4 _EmissionColor;
+half _Cutoff;
+half _Shininess;
+half4 _RimColor;
+half _RimPower;
+sampler2D _RimMatcap;
+
+half4 _HairColor;
+sampler2D _HairTexture;
+half _HairRefRange;
+half _Strength;
+sampler2D _ShiftMap;
+half _ShiftOffset;
+CBUFFER_END
+
+TEXTURE2D(_SpecGlossMap);       SAMPLER(sampler_SpecGlossMap);
+
+half4 SampleSpecularGloss(half2 uv, half alpha, half4 specColor, TEXTURE2D_ARGS(specGlossMap, sampler_specGlossMap))
+{
+	half4 specularGloss = half4(0.0h, 0.0h, 0.0h, 1.0h);
+#ifdef _SPECGLOSSMAP
+	specularGloss = SAMPLE_TEXTURE2D(specGlossMap, sampler_specGlossMap, uv);
+#elif defined(_SPECULAR_COLOR)
+	specularGloss = specColor;
+#endif
+
+#ifdef _GLOSSINESS_FROM_BASE_ALPHA
+	specularGloss.a = alpha;
+#endif
+	return specularGloss;
+}
+
+
+#endif // LIGHTWEIGHT_INPUT_SURFACE_SIMPLE_INCLUDED
